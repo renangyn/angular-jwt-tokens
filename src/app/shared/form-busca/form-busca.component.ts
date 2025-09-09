3377d903e10b681b2b554f6e4c41c90e17c0f964
renangyn <143275789+renangyn@users.noreply.github.com>
2025-09-08 22:06:19 -0300

    first commit

---
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from '../modal/modal.component';
+import { FormBuscaService } from 'src/app/core/services/form-busca.service';
+
+@Component({
+  selector: 'app-form-busca',
+  templateUrl: './form-busca.component.html',
+  styleUrls: ['./form-busca.component.scss']
+})
+export class FormBuscaComponent {
+  constructor( 
+    public formBuscaService : FormBuscaService) {}
+
+    enviar() {
+      console.log(this.formBuscaService.formBusca.value);
+    }
+}
